87069e37a19153e3ee9232a30d06811c6b25d92f
Ivan-49 <remnevvanya@yandex.ru>
2025-03-21 01:41:07 +0300

    добавил файл с функциями для работы с jwt токенами и паролями пользователей

---
+++ src/API/routers/auth/service/security.py
@@ -0,0 +1,59 @@
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from argon2 import PasswordHasher
+from dotenv import load_dotenv
+import os
+
+from routers.auth.service.repository import UserRepository
+
+load_dotenv()
+
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
+argon2 = PasswordHasher(
+    memory_cost=2**16,  
+    parallelism=4,
+    hash_len=32,
+    salt_len=16
+)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    try:
+        argon2.verify(hashed_password, plain_password)
+        return True
+    except:
+        return False
+
+def get_password_hash(password: str) -> str:
+    return argon2.hash(password)
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
+    if not data or not JWT_SECRET_KEY:
+        raise ValueError("Некорректные данные для создания токена")
+
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": int(expire.timestamp())})
+    
+    return jwt.encode(
+        to_encode,
+        JWT_SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+def verify_token(token: str, credentials_exception) -> int:
+    try:
+        payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: int = payload.get("sub")
+        if user_id is None:
+            raise credentials_exception
+        return user_id
+    except JWTError:
+        raise credentials_exception
+
+user_repository = UserRepository()
