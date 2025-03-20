9db9896eb5b1df3bf5b12a130543a0e4f62f9b5d
Ivan-49 <remnevvanya@yandex.ru>
2025-03-21 01:40:13 +0300

    добавил UserRepository и основные методы к нему

---
+++ src/API/routers/auth/service/repository.py
@@ -0,0 +1,66 @@
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from logging import getLogger
+from sqlalchemy.exc import IntegrityError
+from typing import Optional
+
+from models.user import UserModel
+from routers.auth.schemas.user import UserShema
+from database.main import get_session
+
+logger = getLogger(__name__)
+
+
+class UserRepository:
+    async def create_user(
+        self, user: UserShema, session: AsyncSession
+    ) -> UserModel:
+        from routers.auth.service.security import get_password_hash
+        
+        logger.error("Начали создавать пользователя")
+        try:
+            if not user:
+                logger.error("User is None")
+                return None
+            user = user.model_dump()
+            if "password" in user:
+                user["hashed_password"] = get_password_hash(user["password"])
+                del user["password"]
+            user = UserModel(**user)
+
+            session.add(user)
+            await session.commit()
+            logger.info("User created successfully")
+            return user
+        except IntegrityError as e:
+            if "duplicate key" in str(e):
+                logger.error("Пользователь с таким ID или username уже существует")
+                return {"message": "User already existsx"}
+            raise e
+        except Exception as e:
+            logger.error(f"Error create user: {e}")
+            return None
+
+    async def get_user_by_user_id(
+        self, user_id: int, session: AsyncSession ) -> Optional[UserModel]:
+        try:
+            result = await session.execute(
+                select(UserModel).where(UserModel.user_id == user_id)
+            )
+            return result.scalars().first()
+        except Exception as e:
+            logger.error(f"Error get user: {e}")
+            return None
+
+    async def get_user_by_username(
+        self, username: str, session: AsyncSession ) -> Optional[UserModel]:
+        try:
+
+            result = await session.execute(
+                select(UserModel).where(UserModel.username == username)
+            )
+            return result.scalars().first()
+        except Exception as e:
+            logger.error(f"Error get user: {e}")
+            return None
