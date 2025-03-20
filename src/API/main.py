fdd1bd11a9c3c1266058c4744f5be1984eed55d4
Ivan-49 <remnevvanya@yandex.ru>
2025-03-21 01:43:06 +0300

    подключил роутер аутетификации и инициализировал модели sqlalchemy

--- src/API/main.py
+++ src/API/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 import logging
 
+from routers.auth.router import router as auth_router
+from database.main import init_models
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 app = FastAPI()
+
+app.include_router(auth_router, prefix="/api/v1/auth")
+
+
+@app.on_event("startup")
+async def startup_event():
+    await init_models()
+
 
 @app.get("/")
 async def root():
@@ -11,4 +23,5 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run("main:app", host="127.0.0.1", port=80, reload=True)
