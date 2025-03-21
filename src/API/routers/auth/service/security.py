61c9f8e4d6912c32df9de868fb19fb58db7ac428
Ivan-49 <remnevvanya@yandex.ru>
2025-03-21 06:43:57 +0300

    изменил время существования с 15 секунд на значение которое берется из переменной окружения

--- src/API/routers/auth/service/security.py
+++ src/API/routers/auth/service/security.py
@@ -37,7 +37,7 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None) -> s
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": int(expire.timestamp())})
     
     return jwt.encode(
