5944e8fedd951a22aa27fe1a79ba7e489b3c2089
Ivan-49 <remnevvanya@yandex.ru>
2025-03-24 05:17:44 +0300

    добавил: * в функцию сохранения токена в redis проверку типа данных tll * убрал из черного списка redis хранени токена на прямую * в функции которая проверяет есть ли токен в redis убрал получаение самого токена

--- src/API/redis_client.py
+++ src/API/redis_client.py
@@ -16,16 +16,21 @@ class RedisClient:
         self.redis = aioredis.from_url(url, decode_responses=decode_responses)
 
     async def save_token(self, user_id, jwt_token, ttl):
-        await self.redis.set(f"token-{user_id}", jwt_token, expire=ttl)
+        try:
+            if type(ttl) != int:
+                ttl = int(ttl)
+            await self.redis.set(f"token-{user_id}", jwt_token, ex=ttl)
+        except TypeError as e:
+            print(e)
 
     async def get_token(self, user_id):
         return await self.redis.get(f"token-{user_id}")
 
-    async def add_to_blacklist(self, user_id, jwt_token):
-        await self.redis.set(f"blacklist-{user_id}", jwt_token)
+    async def add_to_blacklist(self, user_id, ttl: int = 3600):
+        await self.redis.set(f"blacklist-{user_id}", "revoked", ex=ttl)
 
     async def check_blacklist(self, user_id):
-        return await self.redis.get(f"blacklist-{user_id}")
+        return await self.redis.exists(f"blacklist-{user_id}")
 
     async def close_connection(self):
         await self.redis.close()
