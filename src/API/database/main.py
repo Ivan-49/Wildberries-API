e5e9aa3719212244d7e529a5cbc1f5fe46f08bd4
Ivan-49 <remnevvanya@yandex.ru>
2025-03-29 01:47:55 +0300

    информаци о логировании базы данных подтягиваестся из переменных окружения

--- src/API/database/main.py
+++ src/API/database/main.py
@@ -7,7 +7,8 @@ from database.base import Base
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_async_engine(DATABASE_URL, echo=True)
+ECHO_DB = os.getenv("ECHO_DB")
+engine = create_async_engine(DATABASE_URL, echo=ECHO_DB)
 
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
