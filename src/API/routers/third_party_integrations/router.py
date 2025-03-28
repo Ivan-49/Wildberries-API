6a71c7bc3b332aab8fc460dcfd501084e7373bc0
Ivan-49 <remnevvanya@yandex.ru>
2025-03-29 01:50:14 +0300

    подключение нового роутера, к роутеру работы со сторонними сервисами

--- src/API/routers/third_party_integrations/router.py
+++ src/API/routers/third_party_integrations/router.py
@@ -1,8 +1,16 @@
 from fastapi import APIRouter
 
 from routers.third_party_integrations.service.wb.wb_router import router as wb_router
+from routers.third_party_integrations.service.AI.AI_router import router as giga_chat_router
 
 third_party_router = APIRouter()
 third_party_router.include_router(
-    wb_router, prefix="/wildberries", tags=["wildberries"]
+    router=wb_router,
+    prefix="/wildberries",
+      tags=["wildberries"]
+)
+third_party_router.include_router(
+    router=giga_chat_router,
+    prefix="/ai-analyze",
+    tags=["ai-analyze"]
 )
